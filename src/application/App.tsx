07ee7da712b35c6117ce32495a3268bc7f586e8d
Luan-Fones <luanfones@gmail.com>
2024-08-17 10:21:55 -0300

    ajustes de configuração

---
+++ src/application/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/Application.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Batatinha
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
